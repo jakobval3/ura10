b702fd1f0f55a9b75128716aed29097387f912ac
tilenavs <tilenavs@gmail.com>
2022-10-07 18:35:57 +0200

    works fine

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import random
 
+
 secret = random.randint(1, 30)
-attempts = 0
+attempts= 0
+best_score= None
+print(secret)
+
+
+
+with open("score.txt", "r") as file:
+    best_score= int(file.read())
+    print(f"Top score: {best_score}")
 
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
@@ -10,6 +19,9 @@ while True:
     if guess == secret:
         print(f"You've guessed it - congratulations! It's number {guess}" )
         print(f"Attempts needed: {attempts}")
+        if  best_score > attempts:
+            with open("score.txt", "w") as file:
+                file.write(str(attempts))
         break
     elif guess > secret:
         print("Your guess is not correct... try something smaller")
